3c680536cc51fce484d1dda0b1620126821867af
Anthony Rodriguez <anthony@nezia.dev>
2025-09-02 22:18:59 +0200

    packages: add slippi-launcher wrapper

--- packages/slippi-launcher.nix
+++ packages/slippi-launcher.nix
@@ -1,48 +1,59 @@
-let
-  # static package defaults
-  defaults = {
-    pname = "slippi-launcher";
-    inherit ((import ../hashes.nix).launcher) version hash;
-  };
-in
 {
   lib,
-  appimageTools,
-  fetchurl,
-  makeWrapper,
-  version ? defaults.version,
-  hash ? defaults.hash,
-  pname ? defaults.pname,
+  libredirect,
+  formats,
+  runCommandLocal,
+  slippi-netplay,
+  slippi-playback,
+  slippi-launcher-unwrapped,
+  /*
+    The full list of settings can be found here (remove the set and get and camel case, ie. getIsoPath -> isoPath):
+    https://github.com/project-slippi/slippi-launcher/blob/572c4f94f4c75d5fd94b463920c9464b4ed05aee/src/settings/api.ts#L39-L65
+  */
+  settings ? {
+    isoPath = "/home/nezia/Documents/roms/Super Smash Bros. Melee (USA) (En,Ja) (Rev 2).iso";
+    netplayDolphinPath = lib.getExe slippi-netplay;
+    playbackDolphinPath = lib.getExe slippi-playback;
+    launchMeleeOnPlay = false;
+  },
+  netplayPackage ? slippi-netplay,
+  playbackPackage ? slippi-playback,
 }:
 let
-  # dynamic package defaults
-  src = fetchurl {
-    inherit hash;
-    url = "https://github.com/project-slippi/slippi-launcher/releases/download/v${version}/Slippi-Launcher-${version}-x86_64.AppImage";
-  };
-  appImageContents = appimageTools.extract {
-    inherit pname src version;
-  };
-in
-(import ./common.nix) {
-  inherit
-    lib
-    appimageTools
-    makeWrapper
-    version
-    pname
-    src
-    appImageContents
-    ;
-  extraInstallCommands = ''
-    wrapProgram "$out/bin/${pname}" \
-      --inherit-argv0 \
-      --set QT_QPA_PLATFORM xcb
-    mkdir -p "$out/share/applications"
-    cp -r "${appImageContents}/$(readlink "${appImageContents}/slippi-launcher.png")" "$out/share/applications/"
+  inherit (lib.meta) getExe;
+  inherit (lib.strings) optionalString;
 
-    sed -e '/Icon/d' -e '/Exec/d' "${appImageContents}/slippi-launcher.desktop" > "$out/share/applications/slippi-launcher.desktop"
-    echo "Icon=$out/share/applications/slippi-launcher.png" >> "$out/share/applications/slippi-launcher.desktop"
-    echo "Exec=$out/bin/${pname} %U" >> "$out/share/applications/slippi-launcher.desktop"
-  '';
-}
+  settingsFormat = formats.json { };
+  settingsFile = settingsFormat.generate "slippi-config" { inherit settings; };
+in
+runCommandLocal slippi-launcher-unwrapped.name
+  {
+    inherit (slippi-launcher-unwrapped) pname version meta;
+  }
+  ''
+    mkdir -p "$out/bin"
+    ln -s "${slippi-launcher-unwrapped}/share" "$out/share"
+    ln -s ${getExe slippi-launcher-unwrapped} "$out/bin/slippi-launcher"
+    ${optionalString (settings != { }) ''
+      mv $out/bin/slippi-launcher $out/bin/.slippi-launcher-wrapped
+
+      cat > $out/bin/slippi-launcher <<EOF
+      #!/bin/sh
+      config_dir="\$HOME/.config/Slippi Launcher"
+      settings_path="\$config_dir/Settings"
+
+      mkdir -p "\$config_dir"
+      if [ ! -e "\$settings_path" ]; then
+        cp ${settingsFile} "\$settings_path"
+      fi
+
+      mkdir -p "\$config_dir/playback" "\$config_dir/netplay"
+      ln -sf ${getExe playbackPackage} "\$config_dir/playback/Slippi_Playback-x86_64.AppImage"
+      ln -sf ${getExe netplayPackage} "\$config_dir/netplay/Slippi_Online-x86_64.AppImage"
+
+      exec $out/bin/.slippi-launcher-wrapped "\$@"
+      EOF
+
+      chmod +x $out/bin/slippi-launcher
+    ''}
+  ''
